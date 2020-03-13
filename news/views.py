907ed1737abbd3156923718a7a475b8e77ea232d
recallyourname <vedkalnikita@gmail.com>
2020-03-13 21:37:12 +0600

    mmm??

--- news/views.py
+++ news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Publication
+from django.utils import timezone
 
 def publications_list(request):
-    return render(request, 'news/publications_list.html', {})
+    publications = Publication.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'news/publications_list.html', {'publications':publications})
