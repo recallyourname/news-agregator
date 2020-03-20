03d6b03d73b00818ccdad97f076a1b7b5b864153
recallyourname <vedkalnikita@gmail.com>
2020-03-20 11:17:31 +0600

    some changes

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.publications_list, name = 'publications_list'),
+    path('create/', views.create_publication, name = 'create_publication'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Publication
 from django.utils import timezone
 
 def publications_list(request):
-    publications = Publication.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    publications = Publication.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:5]
     return render(request, 'news/publications_list.html', {'publications':publications})
+
+def create_publication(request):
+    return render(request, 'news/create_publication.html')
+    
