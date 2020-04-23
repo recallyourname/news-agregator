f1b21ec2a9aaabbaa2878d7fae36bc1da244571a
recallyourname <vedkalnikita@gmail.com>
2020-04-23 18:03:20 +0600

    added pagination removed shit

--- news/views.py
+++ news/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 from .models import Publication
 from django.utils import timezone
-
 
 def publication_view(request, pk):
     publication = Publication.objects.get(pk=pk)
     return render(request, 'news/show_publication.html', {'publication':publication})
 
-def publications_list(request):
-    publications = Publication.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:5]
-    return render(request, 'news/publications_list.html', {'publications':publications})
+class publications_list(ListView):
+    model = Publication
+    template_name = 'news/publications_list.html'
+    context_object_name = 'publications'
+    ordering = ['-published_date']
+    paginate_by = 5
 
 def create_publication(request):
     return render(request, 'news/create_publication.html')
-    
